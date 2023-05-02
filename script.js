60260ce8ba089d8b7de6aced183a423d9f108aa0
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-05-02 12:10:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// const element=document.getElementById("colorSelect");
+// colorSelect.eventListner("click" function() {
+	
+// })
+function removeColor() {
+  const selectElement = document.getElementById("colorSelect");
+  const selectedOption = selectElement.options[selectElement.selectedIndex];
+
+  if (selectedOption) {
+    selectElement.removeChild(selectedOption);
+  }
+}
+
+const removeButton = document.querySelector('input[type="button"]');
+removeButton.addEventListener('click', removeColor);
+		
